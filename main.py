7b56fa8b412b2a662596767c26fc88f87e6cf0f3
Alister Animesh Baroi <44337842+AlisterBaroi@users.noreply.github.com>
2023-10-11 23:53:58 +0000

    finally fixed it for good

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import openai
 import os
 
-openai.api_key = process.env.APIKEY
+# openai.api_key = process.env.APIKEY
+openai.api_key = os.environ['APIKEY'] # Stored the API Key as a GitHub Actions Secret 
 
 def AlisterGPT(prompt):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": prompt}]
     )
-
     return response.choices[0].message.content.strip()
 
 if __name__ == "__main__":
+    print("Welcome to AlisterGPT\nNote: Type 'bye', 'exit' or 'quit' to exit the app.\n")
     while True:
-        print("Welcome to AlisterGPT\nNote: Type 'bye', 'exit' or 'quit' to exit the app.")
         inpuT = input("You: ")
+        print()
         if inpuT.lower() in ["quit", "exit", "bye"]:
             break
         response = AlisterGPT(inpuT)
-        print("AlisterGPT: ", response)
+        print("AlisterGPT: ", response, "\n")
 
